d6483a6fce110294c80494ae70dc058363d371c6
zenghyun <pipo36@naver.com>
2023-07-05 21:38:45 +0900

    Work coding test

---
+++ 2._Coding Test/1._Javascript_Syntax/input&output/test4.js
@@ -0,0 +1,13 @@
+// 3자리수 곱셈 문제 2588번
+
+let fs = require("fs");
+
+let input = fs.readFileSync("dev/stdin").toString().split("\n");
+
+let a = input[0];
+let b = input[1];
+
+console.log(Number(a) * Number(b[2]));
+console.log(Number(a) * Number(b[1]));
+console.log(Number(a) * Number(b[0]));
+console.log(Number(a) * Number(b));
