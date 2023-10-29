49adbe46f984b31c20f75d1f04cd638236348abb
zenghyun <pipo36@naver.com>
2023-10-30 00:47:10 +0900

    Refactoring code

--- 2._Coding_Test/Programmers/Lv.1/바탕화면 정리.js
+++ 2._Coding_Test/Programmers/Lv.1/바탕화면 정리.js
@@ -3,6 +3,8 @@
     https://school.programmers.co.kr/learn/courses/30/lessons/161990
  */
 
+
+// ver 1
 function solution(wallpaper) {
   var answer = Array.from(
     { length: wallpaper.length },
@@ -25,7 +27,6 @@ function solution(wallpaper) {
   }
 
   var values = [];
-  console.log(path);
   for (var col = 0; col < path[0].length; col++) {
     var columnValues = path.map((row) => row[col]);
     if (col < 2) {
@@ -40,15 +41,36 @@ function solution(wallpaper) {
   return values;
 }
 
-// console.log(solution([".#...", "..#..", "...#."])) // [0, 1, 3, 4]
-console.log(
-  solution([
-    "..........",
-    ".....#....",
-    "......##..",
-    "...##.....",
-    "....#.....",
-  ])
-); // [1, 3, 5, 8]
-// console.log(solution([".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."])) // [0, 0, 7, 9]
-// console.log(solution(["..", "#."])) // [1, 0, 2, 1]
+// ver 2 
+function solution(wallpaper) {
+    var left = [];
+    var top = [];
+    var right = [];
+    var bottom = [];
+
+    wallpaper.forEach((v,idx) => {
+      [...v].forEach((file, index) => {
+        if(file === "#") {
+          left.push(idx);
+          top.push(index);
+          right.push(idx+1);
+          bottom.push(index+1);
+        }
+      } )
+    })
+    return [Math.min(...left), Math.min(...top), Math.max(...right), Math.max(...bottom)];
+  }
+  
+  // console.log(solution([".#...", "..#..", "...#."])) // [0, 1, 3, 4]
+  console.log(
+    solution([
+      "..........",
+      ".....#....",
+      "......##..",
+      "...##.....",
+      "....#.....",
+    ])
+  ); // [1, 3, 5, 8]
+  // console.log(solution([".##...##.", "#..#.#..#", "#...#...#", ".#.....#.", "..#...#..", "...#.#...", "....#...."])) // [0, 0, 7, 9]
+  // console.log(solution(["..", "#."])) // [1, 0, 2, 1]
+  
